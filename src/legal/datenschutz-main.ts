7fcc1fa21d640c9f8e1772f1771ac517184a8f52
Leon Schulte <leonschulte@Leons-MacBook-Pro.local>
2026-07-16 18:02:10 +0200

    Neuaufbau: Website V2 mit Navigation, Impressum/Datenschutz, lokalen Fonts und Assets

--- src/legal/datenschutz-main.ts
+++
@@ -1,9 +0,0 @@
-import '../app.css';
-import DatenschutzPage from './DatenschutzPage.svelte';
-
-const target = document.getElementById('app');
-if (!target) throw new Error('#app container missing');
-
-const app = new DatenschutzPage({ target });
-
-export default app;
--- src/legal/impressum-main.ts
+++
@@ -1,9 +0,0 @@
-import '../app.css';
-import ImpressumPage from './ImpressumPage.svelte';
-
-const target = document.getElementById('app');
-if (!target) throw new Error('#app container missing');
-
-const app = new ImpressumPage({ target });
-
-export default app;
